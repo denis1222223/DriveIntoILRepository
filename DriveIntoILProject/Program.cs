c7947b791a1ebeb558757a86a0017f25ab8cd730
denis1222223 <denis1222223@mail.ru>
2016-09-12 11:02:43 +0300

    args checking

--- DriveIntoILProject/Program.cs
+++ DriveIntoILProject/Program.cs
@@ -36,15 +36,37 @@ namespace DriveIntoILProject
             if (l < j)
                 GuickSort(a, l, j);
         }
-        static void Main(string[] args)
+
+        private static int ParseArguments(string[] args)
         {
-            int[] a = new int[5] { 1, 7, 4, 2, 9 };
+            int length = 0;
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please enter a numeric argument.");
+                return 0;
+            }          
+            if (!Int32.TryParse(args[0], out length))
+            {
+                Console.WriteLine("You must enter numeric value as argument.");
+                return 0;
+            }
+            return length;
+        }
+
+        static int Main(string[] args)
+        {
+            int length = ParseArguments(args);
+            if (length == 0)
+                return 1;
+
+            int[] a = new int[length] { 1, 7, 4, 2, 9 };
             GuickSort(a, 0, 4);
-            for (int i = 0; i<5; i++)
+            for (int i = 0; i < 5; i++)
             {
                 Console.Write(a[i].ToString(), " ");
             }
             Console.ReadKey();
+            return 0;
         }
     }
 }
