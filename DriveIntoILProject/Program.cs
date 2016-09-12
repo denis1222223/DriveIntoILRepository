53198e157134096aa27f07ff68657254bfa0417c
denis1222223 <denis1222223@mail.ru>
2016-09-13 01:16:50 +0300

    randomizer added

--- DriveIntoILProject/Program.cs
+++ DriveIntoILProject/Program.cs
@@ -41,7 +41,7 @@ namespace DriveIntoILProject
             {
                 Console.WriteLine("Please enter a numeric argument.");
                 return 0;
-            }          
+            }
             if (!Int32.TryParse(args[0], out length))
             {
                 Console.WriteLine("You must enter numeric value as argument.");
@@ -50,17 +50,30 @@ namespace DriveIntoILProject
             return length;
         }
 
+        private static int[] GenerateRandomNumbers(int length, int min = -9, int max = 9)
+        {
+            int[] numbers = new int[length];
+            Random random = new Random();
+            for (int i = 0; i < length; i++)
+            {
+                numbers[i] = random.Next(min, max);
+            }
+            return numbers;
+        }
+
         static int Main(string[] args)
         {
             //int length = ParseArguments(args);
             //if (length == 0)
             //    return 1;
 
-            int[] a = new int[5] { 1, 7, 4, 2, 9 };
-            GuickSort(a, 0, 4);
-            for (int i = 0; i < 5; i++)
+            int[] numbers = GenerateRandomNumbers(10);
+
+            GuickSort(numbers, 0, 9);
+            for (int i = 0; i < 9; i++)
             {
-                Console.Write(a[i].ToString(), ' ');
+                Console.Write(numbers[i].ToString());
+                Console.Write(" ");
             }
             Console.ReadKey();
             return 0;
